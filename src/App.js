8a80f8de3ad059fef4a367a10a838ec9ac0a8b77
Alpish Jain <jalpish@gmail.com>
2021-06-05 10:06:07 +0530

    last section

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import './components/overview';
+import Square from './components/overview';
+import Game from './components/overview';
+function App() {
+  return (
+    <div className="App">
+      <Game />
+    </div>
+  );
+}
+
+export default App;
